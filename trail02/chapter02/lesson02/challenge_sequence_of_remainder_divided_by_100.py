882680db3355837471603ee627b1e47d29baf104
임지혜 <bakcy9842@naver.com>
2025-11-10 12:12:26 +0900

    challenge_sequence_of_remainder_divided_by_100

--- trail02/chapter02/lesson02/challenge_sequence_of_remainder_divided_by_100.py
+++ trail02/chapter02/lesson02/challenge_sequence_of_remainder_divided_by_100.py
@@ -2,6 +2,14 @@
 @see <https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-sequence-of-remainder-divided-by-100/description>
 @problem <challenge_sequence_of_remainder_divided_by_100>
 """
-# def func()
-#
-# n = int(input())
+def func(n):
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 4
+
+    return func(n-2) * func(n-1) % 100
+
+
+n = int(input())
+print(func(n))
