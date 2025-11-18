87a36a158cbd7b68c70cec3f4d3f616d69920b04
임지혜 <bakcy9842@naver.com>
2025-11-19 00:07:46 +0900

    intro_string_sort

---
+++ trail02/chapter03/lesson01/intro_string_sort.py
@@ -0,0 +1,10 @@
+"""
+@see <https://www.codetree.ai/ko/trails/complete/curated-cards/intro-string-sort/description>
+@problem <intro_string_sort>
+"""
+
+
+str = input()
+sorted_arr = sorted(str)
+sorted_str = "".join(sorted_arr)
+print(sorted_str)
